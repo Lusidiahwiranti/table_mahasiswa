b38b340ea27d7abb77a0453458bd1e5ade15052b
Lusidiahwiranti <91247273+Lusidiahwiranti@users.noreply.github.com>
2023-06-06 11:28:52 +0700

    Update api.php

--- api.php
+++ api.php
@@ -14,21 +14,3 @@
     } catch (PDOException $e) {
         echo $e;
     }
-
-    // Endpoint untuk pencarian data mahasiswa berdasarkan nama
-if ($_SERVER['REQUEST_METHOD'] === 'GET' && isset($_GET['search']) && isset($_GET['nama'])) {
-    $nama = $_GET['nama'];
-    try {
-        $sql = "SELECT * FROM mahasiswa WHERE nama LIKE :nama";
-        $stmt = $koneksi->prepare($sql);
-        $stmt->bindValue(':nama', "%$nama%", PDO::PARAM_STR);
-        $stmt->execute();
-        $result = $stmt->fetchAll(PDO::FETCH_ASSOC);
-        header('Content-Type: application/json');
-        echo json_encode($result);
-    } catch(PDOException $e) {
-        echo "Terjadi kesalahan: " . $e->getMessage();
-    }
-}
-
-?>
